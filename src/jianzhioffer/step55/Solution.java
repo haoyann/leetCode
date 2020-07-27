9a19512afd12d363e5af9b3b492a9c67482ba727
haoyann <yanhao@deepexi.com>
2020-07-27 21:53:54 +0800

    二叉树的深度

---
+++ src/jianzhioffer/step55/Solution.java
@@ -0,0 +1,24 @@
+package jianzhioffer.step55;
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        if(root.left==null && root.right==null){
+            return 1;
+        }
+        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+    }
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
