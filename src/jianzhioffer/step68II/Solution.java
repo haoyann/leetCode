56955ba7f2df0a06b21857360f8984e9689dffe7
haoyann <yanhao@deepexi.com>
2020-08-09 23:09:18 +0800

    二叉树的最近公共祖先

---
+++ src/jianzhioffer/step68II/Solution.java
@@ -0,0 +1,28 @@
+package jianzhioffer.step68II;
+
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null || root.val==p.val || root.val==q.val){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+
+        if(left!=null && right!=null){
+            return root;
+        }
+
+        return left != null ? left : right;
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
